de6f2856977dbe76f20e4b02c121aeb565747b7d
dmitriyakkerman <darksider22023103>
2020-10-24 01:07:09 +0300

    Работа по сайдбару, ноутс-бару, подключил попап. создание категорий и создание записей

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
-import './assets//libs/arm-ui/arm-ui.min.js'
 import router from "./router";
 import store from "./store/store";
 import App from './App.vue'
 
 import './assets/libs/arm-ui/arm-ui.min.css'
 import './assets/styles/main.scss'
+
+import './assets/libs/arm-ui/arm-ui.min.js'
 
 Vue.config.productionTip = false
 
