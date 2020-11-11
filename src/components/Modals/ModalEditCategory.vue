3c87bf230a39d649b83813c69270da0e6b1bc12f
dmitriyakkerman <darksider22023103>
2020-11-11 11:36:33 +0200

    доработка модальных окон

--- src/components/Modals/ModalEditCategory.vue
+++ src/components/Modals/ModalEditCategory.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="modal category-modal">
-        <h3 class="modal__title">Note category</h3>
-        <form action="" class="form" @submit.prevent="saveCategory">
-            <div class="form-group">
-                <div class="form__title">
-                    <label for="note-title">Note title</label>
-                    <input id="note-title" type="text" v-model="modalForm.title" ref="title">
+        <div class="modal__container">
+            <h3 class="modal__title">Note category</h3>
+            <form action="" class="form" @submit.prevent="saveCategory">
+                <div class="form-group">
+                    <div class="form__title">
+                        <label for="note-title">Note title</label>
+                        <input id="note-title" type="text" v-model="modalForm.title" ref="title">
+                    </div>
                 </div>
-            </div>
-            <input type="submit" value="Submit" class="form__submit">
-        </form>
-        <a href="" class="modal__close" @click.prevent="closeModal"></a>
+                <input type="submit" value="Submit" class="form__submit">
+            </form>
+            <a href="" class="modal__close" @click.prevent="closeModal"></a>
+        </div>
     </div>
 </template>
 
--- src/components/Modals/ModalEditNote.vue
+++ src/components/Modals/ModalEditNote.vue
@@ -1,35 +1,37 @@
 <template>
     <div class="modal note-modal">
-        <h3 class="modal__title">Note form</h3>
-        <form action="" class="modal-form form" @submit.prevent="saveNote" >
-            <div class="form-group">
-                <div class="form__title">
-                    <label for="note-title">Note title</label>
-                    <input id="note-title" type="text" v-model="modalForm.title" ref="title">
+        <div class="modal__container">
+            <h3 class="modal__title">Note form</h3>
+            <form action="" class="modal-form form" @submit.prevent="saveNote" >
+                <div class="form-group">
+                    <div class="form__title">
+                        <label for="note-title">Note title</label>
+                        <input id="note-title" type="text" v-model="modalForm.title" ref="title">
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="form__select">
-                    <label for="note-select">Note category</label>
-                    <select id="note-select" v-model="modalForm.category.title" ref="category">
-                        <option disabled value="default">Choose category</option>
-                        <option v-for="category in categories" :key="category.id" :value="category.title">{{category.title}}</option>
-                    </select>
+                <div class="form-group">
+                    <div class="form__select">
+                        <label for="note-select">Note category</label>
+                        <select id="note-select" v-model="modalForm.category.title" ref="category">
+                            <option disabled value="default">Choose category</option>
+                            <option v-for="category in categories" :key="category.id" :value="category.title">{{category.title}}</option>
+                        </select>
+                    </div>
+                    <div class="form__date">
+                        <label for="note-date">Deadline</label>
+                        <input type="date" id="note-date" v-model="modalForm.deadline" ref="deadline">
+                    </div>
                 </div>
-                <div class="form__date">
-                    <label for="note-date">Deadline</label>
-                    <input type="date" id="note-date" v-model="modalForm.deadline" ref="deadline">
+                <div class="form-group">
+                    <div class="form__text">
+                        <label for="note-text">Note text</label>
+                        <textarea id="note-text" v-model="modalForm.text" ref="text"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="form__text">
-                    <label for="note-text">Note text</label>
-                    <textarea id="note-text" v-model="modalForm.text" ref="text"></textarea>
-                </div>
-            </div>
-            <input type="submit" value="Submit" class="form__submit">
-        </form>
-        <a href="" class="modal__close" @click.prevent="closeModal"></a>
+                <input type="submit" value="Submit" class="form__submit">
+            </form>
+            <a href="" class="modal__close" @click.prevent="closeModal"></a>
+        </div>
     </div>
 </template>
 
