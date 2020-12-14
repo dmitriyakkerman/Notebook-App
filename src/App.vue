91789c6c853dfd1e68d1b6e2af45a01ec98bf716
dmitriy-akkerman <dmitriy.akkerman.odesseo@gmail.com>
2020-12-14 11:54:25 +0200

    App loader

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app" class="notebook" :class="{'dark-theme': darkTheme}">
 
-    <div class="notebook__container">
-      <Header :sidebarOpen="sidebarOpen" @toggleSidebar="toggleSidebar"></Header>
-      <Sidebar :class="{active: sidebarOpen}" @closeSidebar="toggleSidebar"></Sidebar>
-      <div class="notebook-content">
-        <router-view></router-view>
-      </div>
-    </div>
+    <transition name="appear">
+    <PageLoader v-if="loaded === false"></PageLoader>
+    </transition>
+
+    <transition name="fadeIn">
+        <div class="notebook__container" v-if="loaded">
+          <Header :sidebarOpen="sidebarOpen" @toggleSidebar="toggleSidebar"></Header>
+          <Sidebar :class="{active: sidebarOpen}" @closeSidebar="toggleSidebar"></Sidebar>
+          <div class="notebook-content">
+            <router-view></router-view>
+          </div>
+        </div>
+    </transition>
 
     <Modal></Modal>
 
@@ -22,6 +28,7 @@
 
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
+import PageLoader from "./components/Loaders/PageLoader";
 
 import {mapGetters} from 'vuex'
 
@@ -30,11 +37,13 @@ export default {
   components: {
     Header,
     Sidebar,
+    PageLoader,
     Modal: () => import('./components/Modals/Modal.vue'),
     ModalMessage: () => import('./components/Modals/ModalMessage.vue')
   },
   data() {
     return {
+      loaded: false,
       sidebarOpen: false
     }
   },
@@ -45,6 +54,12 @@ export default {
     toggleSidebar() {
       this.sidebarOpen = !this.sidebarOpen
     }
+  },
+  mounted() {
+    let that = this;
+    setTimeout(() => {
+      that.loaded = true
+    }, 1000)
   }
 }
 
